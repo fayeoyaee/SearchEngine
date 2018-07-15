27041f10cdb51067ef0a25eb07df21ea341a9951
fayeoyaee <ouyang.f@husky.neu.edu>
2018-07-14 17:36:16 -0700

    extract context

--- stockNLP/src/main/java/com/fayeoyaee/Crawler.java
+++ stockNLP/src/main/java/com/fayeoyaee/Crawler.java
@@ -1,12 +1,17 @@
 package com.fayeoyaee;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 import com.hankcs.hanlp.HanLP;
+import com.hankcs.hanlp.seg.common.Term;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -17,6 +22,8 @@ import org.jsoup.Jsoup;
  *
  */
 public class Crawler {
+  public static int CONTEXT_MIN_LEN = 10;
+
   /**
    * Post subclass to store the meta data of individual posts
    */
@@ -32,10 +39,20 @@ public class Crawler {
     }
   }
 
+  private class Context {
+    int start;
+    int end;
+
+    Context(int s, int e) {
+      this.start = s;
+      this.end = e;
+    }
+  }
+
   private Queue<Post> posts = new LinkedList<>();
 
   /**
-   * Given initial_urls, parse and store all the posts 
+   * Given initial_urls, parse and store all the posts
    */
   private void start_requests(List<String> initial_urls) {
     initial_urls.forEach((url) -> {
@@ -44,7 +61,8 @@ public class Crawler {
         JSONArray list = new JSONArray(body.substring(1, body.length() - 1));
         for (int i = 0; i < list.length(); ++i) {
           JSONObject j = list.getJSONObject(i);
-          if (!JSONObject.NULL.equals(j.get("Title")) && !JSONObject.NULL.equals(j.get("Url")) && !JSONObject.NULL.equals(j.get("CreatedTime"))) {
+          if (!JSONObject.NULL.equals(j.get("Title")) && !JSONObject.NULL.equals(j.get("Url"))
+              && !JSONObject.NULL.equals(j.get("CreatedTime"))) {
             posts.add(new Post((String) j.get("Title"), (String) j.get("Url"), (String) j.get("CreatedTime")));
           }
         }
@@ -59,11 +77,47 @@ public class Crawler {
    */
   private void parse(Post p) {
     try {
-      // get the full article
+      // Get the full article
       String text = Jsoup.connect(p.url).get().select(".Article").first().text();
 
-      // NLP lexical parser 
-      System.out.println(HanLP.segment(text));
+      // NLP lexical parser
+      List<Term> term_list = HanLP.segment(text);
+
+      // Remove apostrophes
+      term_list = term_list.stream().filter(term -> !term.nature.toString().equals("w")).collect(Collectors.toList());
+
+      // Extract contexts: Each context are some words before and after the stock
+      // index (tagged "sn") with some minimum length
+      Stack<Context> stack = new Stack<>();
+      for (int i = 0; i < term_list.size(); ++i) {
+        if (term_list.get(i).nature.toString().equals("sn")) {
+          if (!stack.isEmpty()) {
+            Context context = stack.peek();
+            if (context.end < i - CONTEXT_MIN_LEN) {
+              // push new context
+              stack.push(new Context(Math.max(i-CONTEXT_MIN_LEN,0), Math.min(i+CONTEXT_MIN_LEN, term_list.size())));
+            } else {
+              // extend the context
+              stack.pop();
+              stack.push(new Context(context.start, Math.min(i+CONTEXT_MIN_LEN, term_list.size())));
+            }
+          } else {
+              stack.push(new Context(0, Math.min(i+CONTEXT_MIN_LEN, term_list.size())));
+          }
+        }
+      }
+
+      // Extract terms in each context
+      List<List<Term>> context_terms = new ArrayList<>();
+      Iterator<Context> iter = stack.iterator();
+      while (iter.hasNext()) {
+        Context context = iter.next();
+        List<Term> to_add = new ArrayList<>();
+        for (int i = context.start; i < context.end; ++i) {
+          to_add.add(term_list.get(i));
+        }
+        context_terms.add(to_add);
+      }
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -75,7 +129,7 @@ public class Crawler {
     // add posts
     start_requests(initial_urls);
 
-    // parse articles of the posts 
+    // parse articles of the posts
     // TODO: make parse and crawl parallel
     while (!posts.isEmpty()) {
       parse(posts.remove());
